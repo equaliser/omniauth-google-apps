d449581cbbbe19767ef5c1a01f9d14987adf899d
Steve Woodward <steve.paperjam@gmail.com>
2011-12-30 10:04:43 +0000

    Integrating Boy van Amstel's Google Hybrid strategy

--- lib/omniauth/strategies/google_apps.rb
+++ lib/omniauth/strategies/google_apps.rb
@@ -1,5 +1,7 @@
+require 'rack/openid'
 require 'omniauth-openid'
 require 'openid/gapps'
+require 'omniauth-oauth'
 
 module OmniAuth
   module Strategies
@@ -17,6 +19,51 @@ module OmniAuth
       def identifier
         options[:domain] || request['domain']
       end
+      
+      protected
+
+       def dummy_app
+         lambda{|env| [401, {"WWW-Authenticate" => Rack::OpenID.build_header(
+           :identifier => identifier,
+           :return_to => callback_url,
+           :required => @options[:required],
+           :optional => @options[:optional],
+           :"oauth[consumer]" => @options[:consumer_key],
+           :"oauth[scope]" => @options[:scope], 
+           :method => 'post'
+         )}, []]}
+       end
+
+       def auth_hash
+         # Based on https://gist.github.com/569650 by nov
+         oauth_response = ::OpenID::OAuth::Response.from_success_response(@openid_response)
+         
+         consumer = ::OAuth::Consumer.new(
+           @options[:consumer_key],
+           @options[:consumer_secret],
+           :site => 'https://www.google.com',
+           :access_token_path  => '/accounts/OAuthGetAccessToken'
+         )
+         request_token = ::OAuth::RequestToken.new(
+           consumer,
+           oauth_response.request_token,
+           "" # OAuth request token secret is also blank in OpenID/OAuth Hybrid
+         )
+         @access_token = request_token.get_access_token
+         
+         OmniAuth::Utils.deep_merge(super(), {
+           'uid' => @openid_response.display_identifier,
+           'credentials' => {
+             'scope' => @options[:scope], 
+             'token' => @access_token.token,
+             'secret' => @access_token.secret
+           }
+         })
+       
+       end
+   
+      
+      
     end
   end
 end
